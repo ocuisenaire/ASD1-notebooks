66f4a93849467b50f4f707669a3a9b8c77ac6872
Olivier Cuisenaire <olivier.cuisenaire@heig-vd.ch>
2018-09-06 11:29:35 +0200

    Moved too complex code to helpers script

--- 2 - Recursivite/scripts/helpers.py
+++ 2 - Recursivite/scripts/helpers.py
@@ -22,3 +22,31 @@ def grapheComplexite(func,N,title):
     print('Factorielle({0}) :          {1}'.format(X[i],XF[i]))
     print('Rapport C({0})/({0}!-1):      {1}'.format(X[i],C[i]/(XF[i]-1)))
 
+
+def affichageHanoi(Towers,title=None):
+    with plt.xkcd():
+        
+        L = len(Towers[0]) + len(Towers[1]) + len(Towers[2])
+
+        fig = plt.figure()
+        ax = fig.add_axes((0,0,1,0.1*L))
+        
+        for d in range(0,L+1):
+            for i,T in enumerate(Towers):
+                for j,t in enumerate(T):
+                    if d == t:
+                        ax.bar(i,j+1,d/L)
+
+        ax.spines['right'].set_color('none')
+        ax.spines['left'].set_color('none')
+        ax.spines['top'].set_color('none')
+        ax.xaxis.set_ticks_position('bottom')
+        ax.set_xticks([0, 1, 2])
+        ax.set_xlim([-0.5, 2.5])
+        ax.set_ylim([0, L])
+        ax.set_xticklabels(['T1', 'T2', 'T3'])
+        ax.set_yticks([])
+        if title:
+            plt.title(title)
+
+    plt.show()
