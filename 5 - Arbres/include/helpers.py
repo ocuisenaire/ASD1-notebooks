99a43b25bbebe9bd39fd17ad68389c96632985dc
Olivier Cuisenaire <olivier.cuisenaire@heig-vd.ch>
2018-10-02 12:08:43 +0200

    .

--- 5 - Arbres/include/helpers.py
+++ 5 - Arbres/include/helpers.py
@@ -7,7 +7,7 @@ ts.show_leaf_name = False
 ts.show_scale = False
 ts.branch_vertical_margin = 15
 def my_layout(node):
-    F = TextFace(" " + node.name + " ", tight_text=False,fsize=15)
+    F = TextFace(" " + node.name + " ", tight_text=False,fsize=20)
     add_face_to_node(F, node, column=0, position="branch-right")
 ts.layout_fn = my_layout
 
@@ -25,8 +25,32 @@ def newick8_from_binary_tree(R):
         s += "⌀"
     return s
 
+def newick8_from_avl_tree(R):
+    s = ""
+    if R:
+        if R.gauche or R.droite:
+            s += "("
+            s += newick8_from_avl_tree(R.droite)
+            s += ","
+            s += newick8_from_avl_tree(R.gauche)
+            s += ")"
+            s += "{} h{} e{}".format(R.data,R.hauteur,equilibre(R))
+        else:
+            s += "{} h{}".format(R.data,R.hauteur)
+    else:
+        s += "⌀"
+    return s
+
+
+
 def afficher_arbre_binaire(R):
     n = newick8_from_binary_tree(R)+";"
+    # print(n)
+    t = Tree(n,format=8)
+    display(t.render("%%inline", tree_style=ts))
+    
+def afficher_arbre_AVL(R):
+    n = newick8_from_avl_tree(R)+";"
     # print(n)
     t = Tree(n,format=8)
     display(t.render("%%inline", tree_style=ts))
@@ -95,7 +119,7 @@ def inserer(R,val,equilibrer = True):
       
 def meilleurs_cas_AVL():
     R = None
-    for i in range(1:16): 
+    for i in range(1,16): 
         R = inserer(R,i)
     return R
 
